56adb3244db650e398e5118066de6a05ed8a7ed2
MAJIIDMN <muhammadnurmajiid22@gmail.com>
2026-04-23 00:11:21 +0700

    feat: add game configurations for taxes and turn limits

--- src/core/config/GameConfig.cpp
+++ src/core/config/GameConfig.cpp
@@ -1,6 +1,10 @@
 #include "core/config/header/GameConfig.hpp"
 
-GameConfig::GameConfig() : playerCount(4), maxTurns(100), startingMoney(1000000) {}
+#include <algorithm>
+
+GameConfig::GameConfig()
+	: playerCount(4), maxTurns(100), startingMoney(1000000), pphFlat(50000), pphPercentage(10), pbmFlat(50000),
+	  goSalary(200000), jailFine(50000) {}
 
 int GameConfig::getPlayerCount() const {
 	return playerCount;
@@ -14,14 +18,65 @@ int GameConfig::getStartingMoney() const {
 	return startingMoney;
 }
 
+int GameConfig::getPphFlat() const {
+	return pphFlat;
+}
+
+int GameConfig::getPphPercentage() const {
+	return pphPercentage;
+}
+
+int GameConfig::getPbmFlat() const {
+	return pbmFlat;
+}
+
+int GameConfig::getGoSalary() const {
+	return goSalary;
+}
+
+int GameConfig::getJailFine() const {
+	return jailFine;
+}
+
 void GameConfig::setPlayerCount(int value) {
-	playerCount = value;
+	playerCount = std::max(0, value);
 }
 
 void GameConfig::setMaxTurns(int value) {
-	maxTurns = value;
+	maxTurns = std::max(0, value);
 }
 
 void GameConfig::setStartingMoney(int value) {
-	startingMoney = value;
+	startingMoney = std::max(0, value);
+}
+
+void GameConfig::setPphFlat(int value) {
+	pphFlat = std::max(0, value);
+}
+
+void GameConfig::setPphPercentage(int value) {
+	pphPercentage = std::max(0, value);
+}
+
+void GameConfig::setPbmFlat(int value) {
+	pbmFlat = std::max(0, value);
+}
+
+void GameConfig::setGoSalary(int value) {
+	goSalary = std::max(0, value);
+}
+
+void GameConfig::setJailFine(int value) {
+	jailFine = std::max(0, value);
+}
+
+void GameConfig::setTaxConfig(int pphFlatValue, int pphPercentageValue, int pbmFlatValue) {
+	setPphFlat(pphFlatValue);
+	setPphPercentage(pphPercentageValue);
+	setPbmFlat(pbmFlatValue);
+}
+
+void GameConfig::setSpecialConfig(int goSalaryValue, int jailFineValue) {
+	setGoSalary(goSalaryValue);
+	setJailFine(jailFineValue);
 }
